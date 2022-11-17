84a11d73dd357be6b733e7649570813f17c979f0
yashaswinnie96 <yashaswinnie96@gmail.com>
2022-11-17 17:02:24 -0600

    rock, paper and scissors game

---
+++ paper_rock_scissors.py
@@ -0,0 +1,57 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if user_input == 0:
+  print(rock)
+elif user_input == 1:
+  print(paper)
+elif user_input == 2:
+  print(scissors)
+else:
+  print("Please choose a number between 0 to 2")
+
+computer_choice = random.randint(0,2)
+# print(computer_choice)
+if computer_choice == 0:
+  print("Computer chose: " + rock)
+elif computer_choice == 1:
+  print("Computer chose: " + paper)
+else:
+  print("Computer chose: " + scissors)
+
+#decide who won 
+if (user_input == 2 and computer_choice == 0) or (user_input == 1 and computer_choice == 2) or (user_input == 0 and computer_choice == 1):
+  print("You Lose")
+else:
+  print("You Won")
+  
+  
+
