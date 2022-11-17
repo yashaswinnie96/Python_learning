280a3062b1325120755d226d2d774add670018f1
yashaswinnie96 <yashaswinnie96@gmail.com>
2022-11-16 23:20:40 -0600

    bank roulette

---
+++ bank_roulette.py
@@ -0,0 +1,9 @@
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+print(names)
+length_of_string = len(names)
+random_index = random.randint(0,length_of_string)
+# print(random_index)
+buy_meal = names[random_index]
+print(f"{buy_meal} is going to buy the meal today!")
