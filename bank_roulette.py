24e2dd48cdda03daaedb1706a3167f06f160170d
yashaswinnie96 <yashaswinnie96@gmail.com>
2022-11-16 23:29:00 -0600

    second commit for bank roulette

--- bank_roulette.py
+++ bank_roulette.py
@@ -3,7 +3,7 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 print(names)
 length_of_string = len(names)
-random_index = random.randint(0,length_of_string)
+random_index = random.randint(0,length_of_string - 1)
 # print(random_index)
 buy_meal = names[random_index]
 print(f"{buy_meal} is going to buy the meal today!")
