ec714baf30bfd3d668d04bf305415cba3cf3d2ce
yashaswinnie96 <yashaswinnie96@gmail.com>
2022-12-15 00:21:13 -0600

    updated code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,18 +1,32 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def caesar(combined_text, shift_amount, in_direction):
-  final_text = ""
-  for letter in combined_text:
-    position = alphabet.index(letter)
-    if in_direction == "encode":
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
       new_position = position + shift_amount
-    elif in_direction == "decode":
-      new_position = position - shift_amount
-    final_text += alphabet[new_position]
-  print(f"The {in_direction}d text is {final_text}")
+      end_text += alphabet[new_position]
+    else:
+      end_text += char 
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-caesar(combined_text=text, shift_amount=shift, in_direction=direction)
+
+#import art
+#print(art.logo)
+
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26 #the remainder value will be our index value if the shift number is greater than 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if result == "no":
+    should_continue = False
+    print("Good Bye")
+  
